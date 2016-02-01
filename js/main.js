cbf30852c4f3913dd70a46e1acaed2c0d09cc068
Holly Brice <hbrice@uoregon.edu>
2016-01-31 19:35:06 -0800

    hello

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 //creates the angular modeule called myApp
 var app = angular.module('myApp', []);
 
+app.run(function($rootScope) {
+	$rootScope.name = "Ari Lerner";
+});
+
+
 //root controller
-app.controller('PlayerController', ['$scope', function($scope){
+/*app.controller('PlayerController', ['$scope', function($scope){
 	$scope.playing = false;
 	$scope.audio = document.createElement('audio');
 	$scope.audio.src = '/Desktop/WomanWoman.mp3';
@@ -19,17 +24,17 @@ app.controller('PlayerController', ['$scope', function($scope){
 			$scope.stop();
 		});
 	});x
-}]);
+}]);*/
 
 //track our audio element
-app.controller('RelatedController', ['$scope', function($scope){
+/*app.controller('RelatedController', ['$scope', function($scope){
 }]);
 
 app.controller('DemoController', function($scope) {
   $scope.counter = 0;
   $scope.add = function(amount) { $scope.counter += amount; };
   $scope.subtract = function(amount) { $scope.counter -= amount; };
-});
+});*/
 
 
 // app.run(function($rootScope) {
---
+++ startbootstrap-grayscale-1.0.5/js/images.js
@@ -0,0 +1,8 @@
+<script type="text/javascript">
+	$(document).ready(function(){
+	    $('.bookimg').click(function(){
+	        $(this).css('width','100%');  // original width is 500px 
+	    });
+	});
+
+</script>
